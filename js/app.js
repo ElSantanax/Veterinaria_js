aca949669d1efc962ba824798c704c3ea53c48cf
ElSantanax <josesantanaserrano130598@gmail.com>
2025-07-04 10:04:19 -0500

    Refactor: Mejorar la gestión de notificaciones eliminando alertas duplicadas

--- js/app.js
+++ js/app.js
@@ -54,8 +54,16 @@ class Notificacion {
         // Crear notificiación
         const alerta = document.createElement('DIV');
         alerta.classList.add('text-center', 'w-full', 'p-3', 'text-white', 'my-5', 'alert', 'uppercase', 'font-bold', 'text-sm');
+
+        const alertaPrevia = document.querySelector('.alert'); // Eliminar repetición de la alerta
+        alertaPrevia?.remove();
+
         this.tipo === 'error' ? alerta.classList.add('bg-red-500') : alerta.classList.add('bg-green-500'); // Si es de tipo error o no
         alerta.textContent = this.texto; // Agregar el texto de la alerta
         formulario.parentElement.insertBefore(alerta, formulario); // Insertar en el DOM
+
+        setTimeout(() => {
+            alerta.remove();
+        }, 3000);
     }
 }
