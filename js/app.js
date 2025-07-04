2ec0ce81bf0b5aa1fc7ced755c615a6e6a6fd39c
ElSantanax <josesantanaserrano130598@gmail.com>
2025-07-04 08:19:19 -0500

    Refactor: Organizar el código de eventos y funciones para la gestión de citas

--- js/app.js
+++ js/app.js
@@ -4,3 +4,23 @@ const propietarioInput = document.querySelector('#propietario');
 const emailInput = document.querySelector('#email');
 const fechaInput = document.querySelector('#fecha');
 const sintomasInput = document.querySelector('#sintomas');
+
+// Event
+pacienteInput.addEventListener('change', datosCita);
+propietarioInput.addEventListener('change', datosCita);
+emailInput.addEventListener('change', datosCita);
+fechaInput.addEventListener('change', datosCita);
+sintomasInput.addEventListener('change', datosCita);
+
+const citaObj = {
+    paciente: '',
+    propietario: '',
+    email: '',
+    fecha: '',
+    sintomas: '',
+}
+
+// Funciones
+function datosCita(e) {
+    citaObj[e.target.name] = e.target.value;
+}
