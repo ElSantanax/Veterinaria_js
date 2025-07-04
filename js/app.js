3e046635fe9d89abb36c3ba0bebaada08664dcf9
ElSantanax <josesantanaserrano130598@gmail.com>
2025-07-04 09:48:16 -0500

    Refactor: Mejorar la gestión de notificaciones en el formulario de citas

--- js/app.js
+++ js/app.js
@@ -33,8 +33,29 @@ function submitCita(e) {
     e.preventDefault();
 
     if (Object.values(citaObj).some(valor => valor.trim() === '')) {
-        console.log('Todos los campos son obligatorios');
+        new Notificacion({
+            texto: 'Todos los campos son obligatorios',
+            tipo: 'error'
+        })
         return;
     }
     console.log('Enviando...');
+}
+
+class Notificacion {
+    constructor({ texto, tipo }) {
+        this.texto = texto;
+        this.tipo = tipo;
+
+        this.mostrar();
+    }
+
+    mostrar() {
+        // Crear notificiación
+        const alerta = document.createElement('DIV');
+        alerta.classList.add('text-center', 'w-full', 'p-3', 'text-white', 'my-5', 'alert', 'uppercase', 'font-bold', 'text-sm');
+        this.tipo === 'error' ? alerta.classList.add('bg-red-500') : alerta.classList.add('bg-green-500'); // Si es de tipo error o no
+        alerta.textContent = this.texto; // Agregar el texto de la alerta
+        formulario.parentElement.insertBefore(alerta, formulario); // Insertar en el DOM
+    }
 }
