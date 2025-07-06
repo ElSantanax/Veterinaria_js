c418d78aca09c287357dd0a886c800c35f4657d4
ElSantanax <josesantanaserrano130598@gmail.com>
2025-07-06 05:28:30 -0500

    Refactor: Mejorar la funcionalidad de edición de citas y corregir el manejo del botón de editar

--- js/app.js
+++ js/app.js
@@ -16,6 +16,8 @@ fechaInput.addEventListener('change', datosCita);
 sintomasInput.addEventListener('change', datosCita);
 formulario.addEventListener('submit', submitCita)
 
+let editando = false;
+
 const citaObj = {
     id: generarId(),
     paciente: '',
@@ -95,8 +97,8 @@ class AdminCitas {
             btnEditar.classList.add('py-2', 'px-10', 'bg-indigo-600', 'hover:bg-indigo-700', 'text-white', 'font-bold', 'uppercase', 'rounded-lg', 'flex', 'items-center', 'gap-2', 'btn-editar');
             btnEditar.innerHTML = 'Editar <svg fill="none" class="h-5 w-5" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"></path></svg>'
 
-            // const clone = structuredClone(cita);
-            btnEditar.onclick = () => cargarEdicion(cita);
+            const clone = structuredClone(cita);
+            btnEditar.onclick = () => cargarEdicion(clone);
 
             const btnEliminar = document.createElement('button');
             btnEliminar.classList.add('py-2', 'px-10', 'bg-red-600', 'hover:bg-red-700', 'text-white', 'font-bold', 'uppercase', 'rounded-lg', 'flex', 'items-center', 'gap-2');
@@ -137,14 +139,18 @@ function submitCita(e) {
         return;
     }
 
-    citas.agregar({ ...citaObj });
-    id: generarId(),
+    if (editando) {
+        console.log('Editando');
+    } else {
+        citas.agregar({ ...citaObj });
+        new Notificacion({
+            texto: 'Paciente registrado',
+            tipo: 'exito'
+        });
+    }
+
     formulario.reset();
     reiniciarObjetoCita(); // Borrar datos del formulario
-    new Notificacion({
-        texto: 'Paciente registrado',
-        tipo: 'exito'
-    })
 }
 
 function generarId() {
@@ -162,5 +168,15 @@ function reiniciarObjetoCita() {
 }
 
 function cargarEdicion(cita) {
+    Object.assign(citaObj, cita);
+
+    pacienteInput.value = cita.paciente;
+    propietarioInput.value = cita.propietario;
+    emailInput.value = cita.email;
+    fechaInput.value = cita.fecha;
+    sintomasInput.value = cita.sintomas;
+
+    editando = true;
+
     console.log(cita);
 }
