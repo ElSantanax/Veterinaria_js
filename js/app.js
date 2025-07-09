fd4bb6daba4586742b32894ca3518a5893eab73f
ElSantanax <josesantanaserrano130598@gmail.com>
2025-07-09 07:35:28 -0500

    Veterinaria

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+import Notificacion from "./classes/Notificacion.js";
+
 // Selectores
 const pacienteInput = document.querySelector('#paciente');
 const propietarioInput = document.querySelector('#propietario');
@@ -27,32 +29,6 @@ const citaObj = {
     email: '',
     fecha: '',
     sintomas: '',
-}
-
-class Notificacion {
-    constructor({ texto, tipo }) {
-        this.texto = texto;
-        this.tipo = tipo;
-
-        this.mostrar();
-    }
-
-    mostrar() {
-        // Crear notificiación
-        const alerta = document.createElement('DIV');
-        alerta.classList.add('text-center', 'w-full', 'p-3', 'text-white', 'my-5', 'alert', 'uppercase', 'font-bold', 'text-sm');
-
-        const alertaPrevia = document.querySelector('.alert'); // Eliminar repetición de la alerta
-        alertaPrevia?.remove();
-
-        this.tipo === 'error' ? alerta.classList.add('bg-red-500') : alerta.classList.add('bg-green-500'); // Si es de tipo error o no
-        alerta.textContent = this.texto; // Agregar el texto de la alerta
-        formulario.parentElement.insertBefore(alerta, formulario); // Insertar en el DOM
-
-        setTimeout(() => {
-            alerta.remove();
-        }, 3000);
-    }
 }
 
 class AdminCitas {
